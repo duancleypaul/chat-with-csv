d047f6f936720e53ade59da41e90ae80b3e2c478
Duan Cleypaul <duancleypaul@Duans-MacBook-Pro.local>
2024-09-06 10:05:46 -0300

    fix: reset file buffer and remove decorators

--- main.py
+++ main.py
@@ -3,27 +3,24 @@ import pandas as pd
 from langchain_openai import OpenAI
 import streamlit as st
 
-@st.cache_data
-def load_file(csv_file):
-    return pd.read_csv(csv_file)
-
 def main():
     
     st.set_page_config(page_title="Ask your CSV")
     st.header("Ask your CSV 📈")
 
     csv_file = st.sidebar.file_uploader("Upload a CSV file", type="csv")
-    st.session_state["csv_file"] = csv_file
     
     OPENAI_API_KEY = st.sidebar.text_input("OpenAI API Key:", type="password")
     
     if csv_file and OPENAI_API_KEY:
-        df = load_file(csv_file)
+        df = pd.read_csv(csv_file)
         st.write(df)
+
+        csv_file.seek(0) # resetting the file buffer
         
         agent = create_csv_agent(
             OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY),
-            st.session_state["csv_file"],
+            csv_file,
             verbose=True,
             allow_dangerous_code=True
         )
@@ -32,7 +29,7 @@ def main():
         
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
-                st.write(agent.run(user_question))
+                st.write(agent.invoke(user_question)["output"])
 
 if __name__ == "__main__":
     main()
